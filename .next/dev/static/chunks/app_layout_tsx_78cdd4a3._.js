4de90058cdd5fa7b1c5d2158234fe001ef98996c
Willian Custodio Quintino <williancustodioquintino@gmail.com>
2025-12-01 21:52:33 -0300

    fix: add gitignore and postcss config to fix styles

--- .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__4ae45f4a._.css",
-  "static/chunks/node_modules_a9b47191._.js"
-],
-    source: "dynamic"
-});
